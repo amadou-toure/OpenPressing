ab0f9083055c2d83b0581db750765ce610ec5d81
amadou-toure <amadouelcapotoure@gmail.com>
2023-04-05 09:11:23 +0200

    update ClientIndex page

--- open-pressing/src/components/PressingCard.jsx
+++ open-pressing/src/components/PressingCard.jsx
@@ -5,27 +5,64 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Styles from '../styles/PressingCard.module.css'
+import{Grid,Box} from '@mui/material';
+import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
+import Rating from '@mui/material/Rating';
+import { Paper } from '@mui/material';
+import { width } from '@mui/system';
 
 export default function MediaCard(props) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image={props.pressingImage}
-        title="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {props.pressingName}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {props.pressingDescription}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Voir l'emplacement sur la carte</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <Grid item xs={3}>
+      <Paper elevation={3}>
+        <img src={props.pressingImage} className={Styles.image}/>
+        <Box paddingx={1} marginLeft={2}>
+           <Typography component="h2" variant='subtitle1'>{props.pressingName}</Typography>
+        </Box>
+        <Box sx={{
+            display: "flex",
+            alignItems: "center"
+
+        }}
+        marginLeft={2}>
+          <Typography variant='body2' component="p">
+              <ArrowOutwardIcon sx={{width: 19}} /> {props.pressingLocation} km from you
+          </Typography>
+        </Box>
+       <Box sx={{
+        display:'flex',
+        alignItems:'center'
+       }} marginTop={3} marginLeft={2}>
+        <Rating name='read-only' value={3.5} readOnly precision={0.5} size='small' />
+        <Typography variant="body2" component="p" marginLeft={0.5}>3.5</Typography>
+        <Typography variant="body3" component="p" marginLeft={1.5} sx={{backgroundColor:'grey',fontSize:11}}>(655 avis)</Typography>
+       </Box>
+       <Box marginLeft={2}
+       marginBottom={1}>
+        <Typography variant="h6" component="h6" marginTop={0}>A partir de 500 FCFA/Kg</Typography>
+       </Box>
+      </Paper>
+    </Grid>
+   
+    // <Card sx={{ maxWidth: 345 }}>
+    //   <CardMedia
+    //     sx={{ height: 140 }}
+    //     image={props.pressingImage}
+    //     title="green iguana"
+    //   />
+    //   <CardContent>
+    //     <Typography gutterBottom variant="h5" component="div">
+    //       {props.pressingName}
+    //     </Typography>
+    //     <Typography variant="body2" color="text.secondary">
+    //       {props.pressingDescription}
+    //     </Typography>
+    //   </CardContent>
+    //   <CardActions>
+    //     <Button size="small">Voir l'emplacement sur la carte</Button>
+    //     <Button size="small">Learn More</Button>
+    //   </CardActions>
+    // </Card>
   );
 }
--- open-pressing/src/pages/ClientIndex.jsx
+++ open-pressing/src/pages/ClientIndex.jsx
@@ -1,4 +1,8 @@
-import PressingCard from '../components/PressingCard'
+import PressingCard from '../components/PressingCard';
+import { Container } from '@mui/system';
+import { Grid } from '@mui/material';
+import NavBar from '../components/NavBar'
+
 export default function(){
     // const config = {
     //     method: 'GET',
@@ -11,11 +15,22 @@ export default function(){
 
     // console.log(document.cookie)
     return(
-        <>
-            <div>
-                <PressingCard pressingDescription='' pressingImage='' pressingName='American pressing' />
-               <PressingCard pressingImage='' pressingName='Elegance Pressing' pressingDescription='Votre satisfaction, notre mission' />
-            </div>
+        <> <NavBar />
+            <Container>
+             
+                    <Grid container spacing={5}>
+                           <PressingCard pressingLocation='2' pressingImage='https://th.bing.com/th/id/OIP.o8ZRDNFA9f4TdHi9pJaa1wAAAA?pid=ImgDet&rs=1' pressingName='American pressing' />
+                           <PressingCard pressingLocation='5' pressingImage='https://th.bing.com/th/id/OIP.o8ZRDNFA9f4TdHi9pJaa1wAAAA?pid=ImgDet&rs=1' pressingName='American pressing' />
+                           <PressingCard pressingLocation='9' pressingImage='https://th.bing.com/th/id/R.f6077dc4c705fa9dc9239006ab6a365d?rik=89Z8S4KK1Fnhmw&pid=ImgRaw&r=0' pressingName='American pressing' />
+                           <PressingCard pressingLocation='8' pressingImage='' pressingName='American pressing' />
+                           <PressingCard pressingLocation='6' pressingImage='' pressingName='American pressing' />
+                           
+                    </Grid>
+                 
+            
+                
+               
+            </Container>
         </>
     )
 }
